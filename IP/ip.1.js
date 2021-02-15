1ca8ff0b68a757ed4c224978a639390da2aba216
kairongdonga <dongkaireongi@mail.777p.ml>
2021-02-16 07:56:43 +0800

    add

--- IP/ip.1.js
+++ IP/ip.1.js
@@ -2,45 +2,6 @@ if ($response.statusCode != 200) {
   $done(null);
 }
 
-var city0 = "高谭市";
-var isp0 = "Cross-GFW.org";
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-function City_ValidCheck(para) {
-  if(para) {
-    if (para.length<6) {
-      return para
-    } else {
-      return para.slice(0,5)
-    }
-  
-  } else
-  {
-  return city0
-
-  }
-}
-
-function ISP_ValidCheck(para) {
-  if(para) {
-  return para
-  } else
-  {
-  return isp0
-  }
-}
-
-function Area_check(para) {
-  if(para=="中华民国"){
-  return "台湾"
-  } else
-  {
-  return para
-  }
-}
-
 var body = $response.body;
 var obj = JSON.parse(body)["data"];
 flags.get(obj['country']):"🏴‍☠️"
