670285b61cf77b0ede33e3c40c3807d4344c3940
kairongdonga <dongkaireongi@mail.777p.ml>
2021-05-23 08:04:41 +0800

    craft.js

---
+++ Craft/craft.js
@@ -0,0 +1,27 @@
+
+# https:\/\/api.craft.do/auth/profile
+
+
+#https:\/\/res.craft.do/user/full/.*
+
+# hostname= api.craft.do,res.craft.do
+
+
+{
+  "userId": "83879051-715b-fc2e-7a9c-db7df40351fb",
+  "profileImgUrl": "https://res.craft.do/user/full/83879051-715b-fc2e-7a9c-db7df40351fb/4A5FC816-AD5B-47EC-BC1D-483680991CAE_3",
+  "email": "a75avpqcp6@privaterelay.appleid.com",
+  "firstName": "",
+  "lastName": "Mick",
+  "market": "CN",
+  "spaces": [],
+  "subscription": {
+    "tier": "Pro",
+    "subscriptionActive": true,
+    "expirationDate": 1684797896,
+    "subscriptionType": "license",
+    "rawSubscriptionType": "PendingSubscription",
+    "productId": "700-day"
+  },
+  "limits": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Im1heE51bWJlck9mU3BhY2VzIjoxMDAsImRlZmF1bHRTcGFjZUxpbWl0cyI6eyJtYXhTdWJCbG9ja3NJbkJsb2NrIjoyMDAwLCJtYXhEb2N1bWVudHNJblNwYWNlIjoxMDAwLCJtYXhCbG9ja3NJblNwYWNlIjoxMDAwMDAsIm1heE1lbWJlcnNJblNwYWNlIjoyMCwibWF4Q29udGVudExlbmd0aE9mQmxvY2siOjEwMDAwLCJtYXhDb250ZW50TGVuZ3RoT2ZTbmlwcGV0IjoxMDAwMCwibWF4Q29weUFtb3VudCI6MTAwMDAsIm1heEZvbGRlcnNJblNwYWNlIjoxMDAsIm1heExlbmd0aE9mQ29tbWVudCI6MjAwMCwibWF4Q29tbWVudHNJblNwYWNlIjoxMDAwMCwibWF4RmlsZVNpemUiOjI1NiwibWF4QXR0YWNobWVudHNJblNwYWNlIjoxMDAwMCwibWF4TnVtYmVyT2ZEYXlzSW5BY3Rpdml0eUhpc3RvcnkiOjMwLCJhbGxvd0V4cG9ydCI6dHJ1ZSwibWF4U3RvcmFnZVNpemVGb3JTcGFjZSI6MTAyNDB9fSwic3BhY2VzIjpbXSwidGVhbXMiOltdLCJpYXQiOjE2MjE3MjQ2MTJ9.OVGt_nlawRJVufdi3AHZI9ef78RNfsm1lO7oISlmugQfqyQwPhpn8G83eTRHUGIp50ccDm04EKwM5lDxolZyRZnx0ObS48IbXqZvpLoMOEVjFQNjqsTH7JU2utNeuamtTCoZPAK9gc2u67rkMQN238EJMJR33yXWl98F6ZGHfrHOHteBbXVmYaQBYkyUlxLMgKcAK7HNXafUBpE2yyXQeGtQEqRnYNoqphdhozUMwkuJpVhjsPYjX2QIPEJdWL9hNJYtIPALOKRW11CMLudqq5sAU5jKVwAO7-fGaxM1ps-mgjQiyHHRoTsi9CwhghTIi1HCiBZBfbr91KiYicoiVA"
+}
